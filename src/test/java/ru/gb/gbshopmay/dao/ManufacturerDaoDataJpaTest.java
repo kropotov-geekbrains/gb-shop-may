a04a5d0a23741583dec3493d52383e3acc5ca40c
Garik-S <garik.safronov@gmail.com>
2022-05-25 19:39:17 +0300

    hw2

--- src/test/java/ru/gb/gbshopmay/dao/ManufacturerDaoDataJpaTest.java
+++ src/test/java/ru/gb/gbshopmay/dao/ManufacturerDaoDataJpaTest.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Import;
 import ru.gb.gbshopmay.config.ShopConfig;
 import ru.gb.gbshopmay.entity.Manufacturer;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -39,7 +41,40 @@ class ManufacturerDaoDataJpaTest {
         );
     }
 
-    // todo дз findAll, delete, update
-    // todo дз* попробовать через persistenceContext(либо через Autowoired) (через EntityManager)
+    @Test
+    public void findAllTest() {
+        Manufacturer apple = Manufacturer.builder()
+                .name(APPLE_COMPANY_NAME)
+                .build();
+        Manufacturer microsoft = Manufacturer.builder()
+                .name(MICROSOFT_COMPANY_NAME)
+                .build();
 
+        Manufacturer savedManufacturer1 = manufacturerDao.save(apple);
+        Manufacturer savedManufacturer2 = manufacturerDao.save(microsoft);
+        List<Manufacturer> manufacturerList = manufacturerDao.findAll();
+        assertAll(
+                () -> assertEquals(1L, manufacturerList.get(0).getId()),
+                () -> assertEquals(APPLE_COMPANY_NAME, manufacturerList.get(0).getName()),
+                () -> assertEquals(2L, manufacturerList.get(1).getId()),
+                () -> assertEquals(MICROSOFT_COMPANY_NAME, manufacturerList.get(1).getName()),
+                () -> assertEquals(0, manufacturerList.get(0).getVersion()),
+                () -> assertEquals("User", manufacturerList.get(0).getCreatedBy()),
+                () -> assertNotNull(manufacturerList.get(0).getCreatedDate()),
+                () -> assertEquals("User", manufacturerList.get(0).getLastModifiedBy()),
+                () -> assertNotNull(manufacturerList.get(0).getLastModifiedDate())
+        );
+    }
+
+    @Test
+    public void deleteTest() {
+        Manufacturer manufacturer = Manufacturer.builder()
+                .name(APPLE_COMPANY_NAME)
+                .build();
+        Manufacturer savedManufacturer = manufacturerDao.save(manufacturer);
+        assertEquals(1, manufacturerDao.findAll().size());
+
+        manufacturerDao.deleteById(1L);
+        assertEquals(0, manufacturerDao.findAll().size());
+    }
 }
